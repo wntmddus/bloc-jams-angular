03d7085de90bf93a0191a3de23d4b2a454c9d1ac
Seung Yeon Joo <wntmddussla@hotmail.com>
2016-11-03 02:19:53 -0700

    assignment 5 & 6

---
+++ app/_site/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,12 @@
+(function() {
+     function AlbumCtrl() {
+         this.albums = [];
+         for (var i=0; i < 12; i++) {
+            this.albums.push(angular.copy(albumPicasso));
+         }
+     }
+ 
+     angular
+         .module('blocJams')
+         .controller('AlbumCtrl', AlbumCtrl);
+ })();
